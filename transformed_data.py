4c4cf5a68ee3a0ab06e7164edbe42a64cfd3a6ab
Hossein <hosseinabar42@gmail.com>
2023-09-11 15:27:45 +0330

    Add transformed_data.py

---
+++ transformed_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+class TransformedData:
+    cryptocurrencies_df = pd.read_csv('transformed_data/cryptocurrencies.csv')
+    crypto_daily_history_df = pd.read_csv('transformed_data/crypto_daily_history.csv')
+    crypto_tag_df = pd.read_csv('transformed_data/crypto_tags.csv')
+    tags_df = pd.read_csv('transformed_data/tags.csv')
+    dates_df = pd.read_csv('transformed_data/dates.csv')
+    crypto_price_times_df = pd.read_csv('transformed_data/crypto_price_times.csv')
+    crypto_price_types_df = pd.read_csv('transformed_data/crypto_price_types.csv')
+    crypto_historical_prices_df = pd.read_csv('transformed_data/crypto_historical_prices.csv')
+
+
+print("safsd")
+
+
